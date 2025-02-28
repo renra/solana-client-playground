5bb9190bec871321bbfd76aa053d5721cf7e314e
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2025-02-28 14:14:31 +0100

    Sending transactions

--- app/main.tsx
+++ app/main.tsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
-import { JSX, useMemo, useState } from 'react';
+import { JSX, useCallback, useMemo } from 'react';
 import { createRoot } from 'react-dom/client';
-import { clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { clusterApiUrl, PublicKey, Transaction, TransactionInstruction, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { ConnectionProvider, useConnection, useWallet, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import "@solana/wallet-adapter-react-ui/styles.css";
+
+const smartContractProgramId = '3YnxQPUGkyjcN3umsiNKpxMgYEoJRGZPQ7VTLsJUojJ7';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,6 +35,39 @@ const useAccountInfo = () => {
 
 const MainInner = () : JSX.Element => {
   const accountInfo = useAccountInfo()
+  const { connection } = useConnection()
+  const { publicKey, sendTransaction } = useWallet()
+
+  const sendTx = async () => {
+    if(!publicKey || !connection) {
+      console.error('Wallet not connected')
+      return
+    }
+
+    try {
+      const programId = new PublicKey(smartContractProgramId);
+      const programDataAccount = new PublicKey(publicKey);
+      const transaction = new Transaction();
+
+      const instruction = new TransactionInstruction({
+        keys: [
+          {
+            pubkey: programDataAccount,
+            isSigner: false,
+            isWritable: true,
+          },
+        ],
+        programId,
+      });
+
+      transaction.add(instruction);
+
+      const signature = await sendTransaction(transaction, connection);
+      console.log("Transaction Signature:", signature);
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    }
+  }
 
   return (
     <>
@@ -42,6 +77,12 @@ const MainInner = () : JSX.Element => {
 
       { accountInfo.data &&
           <div>Your balance is {accountInfo.data.lamports / LAMPORTS_PER_SOL} SOL</div>
+      }
+
+      { connection && publicKey && 
+          <button onClick={() => { sendTx() }}>
+            Send a Tx
+          </button>
       }
     </>
   )
