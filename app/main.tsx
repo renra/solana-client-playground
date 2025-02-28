d812fb5915f69d5cfae496441560e032b766cd5d
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2025-02-28 13:27:54 +0100

    Connecting through solana

--- app/main.tsx
+++ app/main.tsx
@@ -1,7 +1,10 @@
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
-import { address, createSolanaClient, LAMPORTS_PER_SOL } from 'gill';
-import { JSX, useState } from 'react';
+import { JSX, useMemo, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { clusterApiUrl } from "@solana/web3.js";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,51 +14,57 @@ const queryClient = new QueryClient({
   },
 })
 
-const { rpc } = createSolanaClient({
-  urlOrMoniker: "devnet", // or `mainnet`, `localnet`, etc
-});
+const endpoint = clusterApiUrl("devnet");
+
 
 // const { value: balance } = await rpc.getBalance(wallet).send();
 // console.log(`Balance: ${Number(balance) / LAMPORTS_PER_SOL} SOL`);
 
-const useBalance = (walletAddress: string | undefined) => {
-  return useQuery({
-    queryKey: ['balance', walletAddress],
-    queryFn: async () => {
-      const wallet = address(walletAddress ?? '');
-      return (await rpc.getBalance(wallet).send()).value
-    },
-    enabled: walletAddress !== undefined && walletAddress !== ''
-  })
-}
+// const useBalance = (walletAddress: string | undefined) => {
+//   return useQuery({
+//     queryKey: ['balance', walletAddress],
+//     queryFn: async () => {
+//       const wallet = address(walletAddress ?? '');
+//       return (await rpc.getBalance(wallet).send()).value
+//     },
+//     enabled: walletAddress !== undefined && walletAddress !== ''
+//   })
+// }
 
 const MainInner = () : JSX.Element => {
   const [walletAddress, setWalletAddress] = useState<string | undefined>(undefined)
-  const balance = useBalance(walletAddress)
+  // const balance = useBalance(walletAddress)
 
-  balance.isLoading
+  // balance.isLoading
+      // { balance.isLoading && <div>...</div> }
+
+      // { balance.data &&  
+      //     <div>
+      //       Your balance is {`${Number(balance.data) / LAMPORTS_PER_SOL} SOL`}
+      //     </div>
+      // }
 
   return (
     <div>
-      <div>
-        <input type="text" placeholder="Wallet address" onChange={(e) => { setWalletAddress(e.target.value == '' ? undefined : e.target.value) }} value={walletAddress || ''} />
-      </div>
-
-      { balance.isLoading && <div>...</div> }
-
-      { balance.data &&  
-          <div>
-            Your balance is {`${Number(balance.data) / LAMPORTS_PER_SOL} SOL`}
-          </div>
-      }
+      Hello!
     </div>
   )
 }
 
 const Main = () : JSX.Element => {
+  const wallets = useMemo(() => [], [])
+
   return (
     <QueryClientProvider client={queryClient}>
-      <MainInner />
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets}>
+          <WalletModalProvider>
+            <WalletMultiButton />
+
+            <MainInner />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </QueryClientProvider>
   )
 }
